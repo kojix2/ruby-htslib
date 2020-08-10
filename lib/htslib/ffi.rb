72f0cd1865e064d83b9ae52116cf37f52f87b043
kojix2 <2xijok@gmail.com>
2020-08-10 20:39:10 +0900

    Fix union_layout and add struct_layout

--- lib/htslib/ffi.rb
+++ lib/htslib/ffi.rb
@@ -4,6 +4,10 @@
 module FFI
   class Struct
     def self.union_layout(*args)
+      Class.new(FFI::Union) { layout(*args) }
+    end
+
+    def self.struct_layout(*args)
       Class.new(FFI::Struct) { layout(*args) }
     end
   end
