4f4a33289c803d8cb0198d71ca1b6ec05d1c6722
kojix2 <2xijok@gmail.com>
2020-08-19 16:47:53 +0900

    Improved kfunc in ffi

--- lib/hts/ffi.rb
+++ lib/hts/ffi.rb
@@ -1131,12 +1131,38 @@ module HTS
 
     # kfunc
 
-    attach_function   :kf_lgamma,               [:double],                                             :double
-    attach_function   :kf_erfc,                 [:double],                                             :double
-    attach_function   :kf_gammap,               %i[double double],                                     :double
-    attach_function   :kf_gammaq,               %i[double double],                                     :double
-    attach_function   :kf_betai,                %i[double double double],                              :double
-    attach_function   :kt_fisher_exact,         %i[int int int int pointer pointer pointer],           :double
+    # Log gamma function
+    attach_function \
+      :kf_lgamma,
+      [:double],
+      :double
+
+    # complementary error function
+    attach_function \
+      :kf_erfc,
+      [:double],
+      :double
+
+    # The following computes regularized incomplete gamma functions.
+    attach_function \
+      :kf_gammap,
+      %i[double double],
+      :double
+
+    attach_function \
+      :kf_gammaq,
+      %i[double double],
+      :double
+
+    attach_function \
+      :kf_betai,
+      %i[double double double],
+      :double
+
+    attach_function \
+      :kt_fisher_exact,
+      %i[int int int int pointer pointer pointer],
+      :double
 
     # hts
 
