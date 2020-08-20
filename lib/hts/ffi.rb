a3bdb5eeb4845d9f71ce56a28d772a3098a76904
kojix2 <2xijok@gmail.com>
2020-08-20 12:59:51 +0900

    Move FFI::Struct to a new file

--- lib/hts/ffi.rb
+++ lib/hts/ffi.rb
@@ -1,17 +1,6 @@
 # frozen_string_literal: true
 
-# This should be removed if you get the better way...
-module FFI
-  class Struct
-    def self.union_layout(*args)
-      Class.new(FFI::Union) { layout(*args) }
-    end
-
-    def self.struct_layout(*args)
-      Class.new(FFI::Struct) { layout(*args) }
-    end
-  end
-end
+require_relative 'ffi/struct'
 
 module HTS
   module FFI
---
+++ lib/hts/ffi/struct.rb
@@ -0,0 +1,12 @@
+# This should be removed if you get the better way...
+module FFI
+  class Struct
+    def self.union_layout(*args)
+      Class.new(FFI::Union) { layout(*args) }
+    end
+
+    def self.struct_layout(*args)
+      Class.new(FFI::Struct) { layout(*args) }
+    end
+  end
+end
