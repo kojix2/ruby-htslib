3e39df7f6d29cc9f75b850bf3f050a21b0235717
kojix2 <2xijok@gmail.com>
2021-01-08 09:11:27 +0900

    imporove test

--- spec/htslib_spec.rb
+++ spec/htslib_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe HTS do
     expect(HTS::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
+  it 'call hts_version' do
     expect(HTS::FFI.hts_version).not_to be nil
   end
 end
