bcb559b1b5eb7d30bf14f39acddfc8cef62445f2
kojix2 <2xijok@gmail.com>
2021-01-15 19:51:41 +0900

    Add test files

---
+++ test/bam/alignment_test.rb
@@ -0,0 +1,4 @@
+require_relative '../test_helper'
+
+class AlignmentTest < Minitest::Test
+end
--- test/bam_test.rb
+++ test/bam_test.rb
@@ -1,4 +1,11 @@
 require_relative 'test_helper'
 
 class BamTest < Minitest::Test
+  def setup
+    @bam = HTS::Bam.new(File.expand_path("assets/poo.sort.bam",__dir__))
+  end
+
+  def test_initialize
+    assert_instance_of HTS::Bam, @bam
+  end
 end
