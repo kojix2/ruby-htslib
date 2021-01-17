e77b2af568481358d9cb2d5cf9711293f3897807
kojix2 <2xijok@gmail.com>
2021-01-17 13:24:09 +0900

    Improved Alignment [wip]

--- lib/hts/bam/alignment.rb
+++ lib/hts/bam/alignment.rb
@@ -26,9 +26,9 @@ module HTS
       end
 
       # Set (query) name.
-      def qname=(_name)
-        raise 'NotImplemented'
-      end
+      # def qname=(name)
+      #   raise 'Not Implemented'
+      # end
 
       def rnext
         tid = @b[:core][:mtid]
@@ -49,25 +49,47 @@ module HTS
       end
 
       def strand
-        FFI.bam_is_rev(@b) ? '-' : '+' 
+        FFI.bam_is_rev(@b) ? '-' : '+'
       end
 
       def base_qualities
+        q_ptr = FFI.bam_get_qual(@b)
+        q_ptr.read_array_of_uint8(@b[:core][:l_qseq])
       end
 
-      def pos; end
+      def pos
+        @b[:core][:pos]
+      end
 
-      def pos=; end
+      # def pos=(v)
+      #   raise 'Not Implemented'
+      # end
 
-      def isize; end
+      def isize
+        @b[:core][:isize]
+      end
 
-      def mapping_quality; end
+      def mapping_quality
+        @b[:core][:qual]
+      end
 
-      def cigar; end
+      def cigar
+        Cigar.new(FFI.bam_get_cigar(@b), @b[:core][:n_cigar])
+      end
 
-      def qlen; end
+      def qlen
+        FFI.bam_cigar2qlen(
+          @b[:core][:n_cigar],
+          FFI.bam_get_cigar(@b)
+        )
+      end
 
-      def rlen; end
+      def rlen
+        FFI.bam_cigar2rlen(
+          @b[:core][:n_cigar],
+          FFI.bam_get_cigar(@b)
+        )
+      end
 
       def seqs; end
 
--- test/bam/alignment_test.rb
+++ test/bam/alignment_test.rb
@@ -3,26 +3,54 @@ require_relative '../test_helper'
 class AlignmentTest < Minitest::Test
   def setup
     @bam = HTS::Bam.new(File.expand_path('../assets/poo.sort.bam', __dir__))
-    @alignment = @bam.first
+    @alm1 = @bam.first
   end
 
   def test_qname
-    assert_equal 'poo_3290_3833_2:0:0_2:0:0_119', @alignment.qname
+    assert_equal 'poo_3290_3833_2:0:0_2:0:0_119', @alm1.qname
   end
 
   def test_rnext
-    assert_equal 'poo', @alignment.rnext
+    assert_equal 'poo', @alm1.rnext
   end
 
   def test_pnext
-    assert_equal 3289, @alignment.pnext
+    assert_equal 3289, @alm1.pnext
   end
 
   def test_rname
-    assert_equal "poo", @alignment.rname
+    assert_equal 'poo', @alm1.rname
   end
 
   def test_strand
-    assert_equal '+', @alignment.strand
+    assert_equal '+', @alm1.strand
+  end
+
+  def test_base_qualities
+    assert_equal ([17] * 70), @alm1.base_qualities
+  end
+
+  def test_pos
+    assert_equal 3289, @alm1.pos
+  end
+
+  def test_isize
+    assert_equal 0, @alm1.isize
+  end
+
+  def test_mapping_quality
+    assert_equal 0, @alm1.mapping_quality
+  end
+
+  def test_cigar
+    assert_instance_of HTS::Bam::Cigar, @alm1.cigar
+  end
+
+  def test_qlen
+    assert_equal 0, @alm1.qlen # Is that right?
+  end
+
+  def test_rlen
+    assert_equal 0, @alm1.rlen # Is that right?
   end
 end
