a3372ed8a4993d7f2ad00a12c6baccc24c4f087d
kojix2 <2xijok@gmail.com>
2021-01-18 20:43:27 +0900

    Add bam example

---
+++ examples/bam.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'htslib'
+
+bam_path = ARGV[0] || File.expand_path('../test/assets/poo.sort.bam', __dir__)
+
+bam = HTS::Bam.new(bam_path)
+
+bam.each do |aln|
+  p name: aln.qname,
+    flag: aln.flag,
+    pos: aln.pos + 1,
+    mpos: aln.pnext + 1,
+    mqual: aln.mapping_quality,
+    seq: aln.seq,
+    cigar: aln.cigar.to_s,
+    qual: aln.base_qualities.map { |i| (i + 33).chr }.join
+end
