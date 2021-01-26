57be2e8ef39776abe1bbe6ef1366c5ae204f84e0
kojix2 <2xijok@gmail.com>
2021-01-26 16:42:27 +0900

    Add some methods to Fai

--- lib/hts/fai.rb
+++ lib/hts/fai.rb
@@ -6,7 +6,7 @@
 module HTS
   class Fai
     def self.open(path)
-      fai = self.new(path)
+      fai = new(path)
       if block_given?
         yield(fai)
         fai.close
@@ -17,21 +17,35 @@ module HTS
 
     def initialize(path)
       @path = File.expand_path(path)
-      @path.delete_suffix!(".fai")
-      if File.exist?(@path + ".fai")
-        FFI.fai_build(@path)
-      end
+      @path.delete_suffix!('.fai')
+      FFI.fai_build(@path) if File.exist?(@path + '.fai')
       @fai = FFI.fai_load(@path)
       # at_exit{FFI.fai_destroy(@fai)}
     end
 
-    def [](region)
-      
+    # the number of sequences in the index.
+    def size
+      FFI.faidx_nseq(@fai)
+    end
+    alias length size
+
+    # return the length of the requested chromosome.
+    def chrom_size(chrom)
+      raise ArgumentError, 'Expect chrom to be String or Symbol' unless chrom.is_a?(String) || chrom.is_a?(Symbol)
+
+      chrom = chrom.to_s
+      result = FFI.faidx_seq_len(@fai, chrom)
+      result == -1 ? nil : result
+    end
+    alias chrom_length chrom_size
+
+    # FIXME: naming and syntax
+    def cget()
     end
 
-    def nseqs; end
-
-    def include?; end
+    # FIXME: naming and syntax
+    def get
+    end
 
     # __iter__
   end
--- test/fai_test.rb
+++ test/fai_test.rb
@@ -8,4 +8,26 @@ class FaiTest < Minitest::Test
   def test_initialize_fai
     assert_instance_of HTS::Fai, @fai
   end
+
+  def test_size
+    assert_equal 5, @fai.size
+  end
+
+  def test_length
+    assert_equal 5, @fai.length
+  end
+
+  def test_chrom_size
+    assert_equal 500, @fai.chrom_size('chr1')
+    assert_equal 500, @fai.chrom_size(:chr1)
+    assert_raises(ArgumentError){ @fai.chrom_size(nil) }
+    assert_nil @fai.chrom_size('chr')
+  end
+
+  def test_chrom_length
+    assert_equal 500, @fai.chrom_length('chr1')
+    assert_equal 500, @fai.chrom_length(:chr1)
+    assert_raises(ArgumentError){ @fai.chrom_length(nil) }
+    assert_nil @fai.chrom_length('chr')
+  end
 end
