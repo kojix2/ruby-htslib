582fab5e90830d0c79715c8dea4c1f4b62d9740f
kojix2 <2xijok@gmail.com>
2021-03-14 21:26:19 +0900

    Move sam constants

--- lib/hts/ffi/sam.rb
+++ lib/hts/ffi/sam.rb
@@ -2,6 +2,24 @@
 
 module HTS
   module FFI
+    # constants
+    BAM_CMATCH     = 0
+    BAM_CINS       = 1
+    BAM_CDEL       = 2
+    BAM_CREF_SKIP  = 3
+    BAM_CSOFT_CLIP = 4
+    BAM_CHARD_CLIP = 5
+    BAM_CPAD       = 6
+    BAM_CEQUAL     = 7
+    BAM_CDIFF      = 8
+    BAM_CBACK      = 9
+
+    BAM_CIGAR_STR = 'MIDNSHP=XB'
+    BAM_CIGAR_STR_PADDED = 'MIDNSHP=XB??????'
+    BAM_CIGAR_SHIFT = 4
+    BAM_CIGAR_MASK  = 0xf
+    BAM_CIGAR_TYPE  = 0x3C1A7
+
     # macros
     class << self
       def bam_cigar_op(c)
@@ -24,6 +42,19 @@ module HTS
         BAM_CIGAR_TYPE >> (o << 1) & 3
       end
     end
+
+    BAM_FPAIRED        =    1
+    BAM_FPROPER_PAIR   =    2
+    BAM_FUNMAP         =    4
+    BAM_FMUNMAP        =    8
+    BAM_FREVERSE       =   16
+    BAM_FMREVERSE      =   32
+    BAM_FREAD1         =   64
+    BAM_FREAD2         =  128
+    BAM_FSECONDARY     =  256
+    BAM_FQCFAIL        =  512
+    BAM_FDUP           = 1024
+    BAM_FSUPPLEMENTARY = 2048
 
     # macros
     # function-like macros
