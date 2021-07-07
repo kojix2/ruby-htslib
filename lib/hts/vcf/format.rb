003e29b10d8a6ffd42d212245467ae3dcba3ce4b
kojix2 <2xijok@gmail.com>
2021-07-07 11:39:22 +0900

    Added skeleton of VCF::Info and VCF::Format

---
+++ lib/hts/vcf/format.rb
@@ -0,0 +1,18 @@
+module HTS
+  class VCF
+    class Format
+      def initialize; end
+
+      def delete; end
+
+      def get; end
+
+      def set; end
+
+      # def fields # iterator
+      # end
+
+      def genotypes; end
+    end
+  end
+end
---
+++ lib/hts/vcf/info.rb
@@ -0,0 +1,18 @@
+module HTS
+  class VCF
+    class Info
+      def initialize; end
+
+      def has_flag?; end
+
+      def get; end
+
+      def set; end
+
+      def delete; end
+
+      # def fields # iterator
+      # end
+    end
+  end
+end
