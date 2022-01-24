30cf404324f08896667693aa7d1c13578ffb4cb6
kojix2 <2xijok@gmail.com>
2022-01-24 14:50:19 +0900

    Improved bam-filter example
    support for multi threads

--- examples/bam-filter.rb
+++ examples/bam-filter.rb
@@ -7,16 +7,32 @@ require "htslib"
 OptionParser.new do |parser|
   parser.banner = "Usage: #{File.basename(__FILE__)} [parserions] <bam_file>"
   parser.on("-e", "--expression EXPR", "eval code from arg") { |v| @expr = v }
-  # parser.on("-t", "--threads NUM") { |v| p v }
+  parser.on("-t", "--threads NUM", Integer) { |v| @threads = v }
   # parser.on("-f", "--fasta PATH") { |v| p v }
   parser.on("-d", "--debug", "print expression") { @debug = true }
   parser.parse!(ARGV) # make it outside the scope of eval.
 end
 
-exit(1) if ARGV.size != 1
+if ARGV.size != 1
+  warn "Please specify a file"; exit(1)
+elsif @expr.nil?
+  warn "Expression is required"; exit(1)
+end
 
 # FIXME: CRAM
 bam = HTS::Bam.open(ARGV[0])
+
+# multi threads
+if @threads
+  thp = HTS::LibHTS::HtsTpool.new
+  thp[:pool] = (t = HTS::LibHTS.hts_tpool_init(@threads))
+  HTS::LibHTS.hts_set_opt(
+    bam, # to_ptr is difined in Bam
+    HTS::LibHTS::HtsFmtOption[:HTS_OPT_THREAD_POOL],
+    :pointer,
+    thp
+  )
+end
 
 @expr = String.new.tap do |s|
   # make it outside the scope of eval.
