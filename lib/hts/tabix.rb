b5f3a818a2eb0c23e8d64546fa89eeee61176d8b
kojix2 <2xijok@gmail.com>
2022-03-27 23:03:19 +0900

    Improved tabix [wip]

--- lib/hts/tabix.rb
+++ lib/hts/tabix.rb
@@ -1,30 +1,60 @@
-# frozen_string_literal: true
-
-# Based on hts-python
-# https://github.com/quinlan-lab/hts-python
-
 require_relative "../htslib"
 
 module HTS
   class Tabix
-    class << self
-      alias open new
+    include Enumerable
+
+    attr_reader :file_path
+
+    def self.open(*args, **kw)
+      file = new(*args, **kw) # do not yield
+      return file unless block_given?
+
+      begin
+        yield file
+      ensure
+        file.close
+      end
+      file
     end
-    def initialize
-      # IO like API
+
+    def initialize(filename, threads: nil)
       if block_given?
-        begin
-          yield self
-        ensure
-          close
-        end
+        message = "HTS::Tabix.new() dose not take block; Please use HTS::Tabix.open() instead"
+        raise message
+      end
+
+      @file_path = filename == "-" ? "-" : File.expand_path(filename)
+
+      if mode[0] == "r" && !File.exist?(file_path)
+        message = "No such Tabix file - #{file_path}"
+        raise message
+      end
+
+      @mode     = "r"
+      @hts_file = LibHTS.hts_open(file_path, @mode)
+
+      if threads&.> 0
+        r = LibHTS.hts_set_threads(@hts_file, threads)
+        raise "Failed to set number of threads: #{threads}" if r < 0
       end
     end
 
-    def build; end
+    def struct
+      @hts_file
+    end
 
-    def sequences; end
+    def to_ptr
+      @hts_file.to_ptr
+    end
 
-    # def __call__\
+    def close
+      LibHTS.hts_close(@hts_file)
+      @hts_file = nil
+    end
+
+    def closed?
+      @hts_file.nil?
+    end
   end
 end
