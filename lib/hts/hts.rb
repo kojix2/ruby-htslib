949aefca28c6efebfcc63f7b2dffb45404953c96
kojix2 <2xijok@gmail.com>
2022-04-04 14:22:35 +0900

    Add seek/tell to HTS
    Fix #14

--- lib/hts/hts.rb
+++ lib/hts/hts.rb
@@ -14,6 +14,22 @@ module HTS
       LibHTS.hts_get_format(@hts_file)[:format].to_s
     end
 
+    def seek(offset)
+      if format == "cram"
+        LibHTS.cram_seek(@hts_file[:fp][:cram], offset, IO::SEEK_SET)
+      else
+        LibHTS.bgzf_seek(@hts_file[:fp][:bgzf], offset, IO::SEEK_SET)
+      end
+    end
+
+    def tell
+      if format == "cram"
+        LibHTS.cram_tell(@hts_file[:fp][:cram])
+      else
+        LibHTS.bgzf_tell(@hts_file[:fp][:bgzf])
+      end
+    end
+
     def format_version
       v = LibHTS.hts_get_format(@hts_file)[:version]
       major = v[:major]
