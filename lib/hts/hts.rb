d0f0b3248dd3c94afc2fc520d506ff490e075c5f
kojix2 <2xijok@gmail.com>
2022-04-05 19:43:01 +0900

    Implement tell and seek

--- lib/hts/hts.rb
+++ lib/hts/hts.rb
@@ -26,24 +26,31 @@ module HTS
     end
     
     def seek(offset)
-      if format == "cram"
+      if @hts_file[:is_cram] == 1
         LibHTS.cram_seek(@hts_file[:fp][:cram], offset, IO::SEEK_SET)
-      else
+      elsif @hts_file[:is_bgzf] == 1
         LibHTS.bgzf_seek(@hts_file[:fp][:bgzf], offset, IO::SEEK_SET)
+      else
+        LibHTS.hseek(@hts_file[:fp][:hfile], offset, IO::SEEK_SET)
       end
     end
 
     def tell
-      if format == "cram"
+      if @hts_file[:is_cram] == 1
         # LibHTS.cram_tell(@hts_file[:fp][:cram])
-      else
+        # warn 'cram_tell is not implemented in c htslib'
+        nil
+      elsif @hts_file[:is_bgzf] == 1
         LibHTS.bgzf_tell(@hts_file[:fp][:bgzf])
+      else
+        LibHTS.htell(@hts_file[:fp][:hfile])
       end
     end
 
     def rewind
-      seek(@start_position) if @start_position
-      # raise error unless @start_position
+      r = seek(@start_position) if @start_position
+      raise "Failed to rewind: #{r}" if r < 0
+      r 
     end
   end
 end
