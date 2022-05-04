629cc0b2eb031c4ca465297763342679f6649466
kojix2 <2xijok@gmail.com>
2022-05-04 23:19:08 +0900

    Update example

--- examples/bcf.rb
+++ examples/bcf.rb
@@ -2,23 +2,17 @@
 
 require "htslib"
 
-path = ARGV[0]
-if path.nil?
-  warn "bcf file not found"
-  warn "e.g.  ruby examples/bcf.rb htslib/test/tabix/vcf_file.bcf"
-  exit
-end
-
-bcf_path = File.expand_path(path)
+bcf_path = File.expand_path("../htslib/test/tabix/vcf_file.bcf", __dir__)
 
 HTS::Bcf.open(bcf_path) do |bcf|
   bcf.each do |r|
-    p chrom: r.chrom,
-      pos: r.pos,
-      id: r.id,
-      qual: r.qual.round(2),
-      ref: r.ref,
-      alt: r.alt,
-      filter: r.filter
+    pp  chrom: r.chrom,
+        pos: r.pos,
+        id: r.id,
+        qual: r.qual.round(2),
+        ref: r.ref,
+        alt: r.alt,
+        filter: r.filter,
+        info: r.info.to_h,
   end
 end
