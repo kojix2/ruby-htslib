404ea289973b6b6ad6cbfed4cc87f3d4da9f94b9
kojix2 <2xijok@gmail.com>
2022-05-25 11:55:53 +0900

    Improved scripts

--- scripts/func_report.sh
+++ scripts/func_report.sh
@@ -9,6 +9,6 @@ echo -e "# FUNC_REPORT\n" > FUNC_REPORT.md
 for file in ${files[@]}; do
   echo -e "## $file\n"                             >> FUNC_REPORT.md
   echo "\`\`\`diff"                                >> FUNC_REPORT.md
-  diff <(./func_h.sh $file) <(./func_ffi.sh $file) >> FUNC_REPORT.md
-  echo -e "\`\`\`\n"                                    >> FUNC_REPORT.md
+  diff <(./func_ffi.sh $file) <(./func_h.sh $file) >> FUNC_REPORT.md
+  echo -e "\`\`\`\n"                               >> FUNC_REPORT.md
 done
