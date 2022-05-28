20e3fe582f2ffaa778d05bbd6ff784c534292778
kojix2 <2xijok@gmail.com>
2022-05-28 17:11:09 +0900

    Rubocop style correct

--- scripts/c2ffi/func2rb.rb
+++ scripts/c2ffi/func2rb.rb
@@ -4,8 +4,9 @@ d = JSON[ARGF.read]
 
 d.each do |h|
   next if h["tag"] != "function"
-  args = h.dig('parameters').map{|pm| pm.dig('type', 'tag')}
-  rt = h.dig('return-type', 'tag')
+
+  args = h.dig("parameters").map { |pm| pm.dig("type", "tag") }
+  rt = h.dig("return-type", "tag")
   puts <<~EOS
     attach_function \\
       :#{h['name']},
