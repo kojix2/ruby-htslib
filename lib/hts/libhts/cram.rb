cb5d06768b49366f3746261f71879677473343f5
kojix2 <2xijok@gmail.com>
2022-05-29 15:49:29 +0900

    Add functions to CRAM

--- lib/hts/libhts/cram.rb
+++ lib/hts/libhts/cram.rb
@@ -3,6 +3,8 @@
 module HTS
   module LibHTS
     typedef :pointer, :cram_fd
+    typedef :pointer, :cram_container
+    typedef :pointer, :cram_block
 
     attach_function \
       :cram_fd_get_header,
@@ -42,196 +44,196 @@ module HTS
       [:cram_fd, HFile],
       :void
 
-    # attach_function \
-    #  :cram_container_get_length,
-    #  [CramContainer],
-    #  :int32
+    attach_function \
+     :cram_container_get_length,
+     [:cram_container],
+     :int32
 
-    # attach_function \
-    #   :cram_container_set_length,
-    #   [CramContainer, :int32],
-    #   :void
+    attach_function \
+      :cram_container_set_length,
+      [:cram_container, :int32],
+      :void
 
-    # attach_function \
-    #   :cram_container_get_num_blocks,
-    #   [CramContainer],
-    #   :int32
+    attach_function \
+      :cram_container_get_num_blocks,
+      [:cram_container],
+      :int32
 
-    # attach_function \
-    #   :cram_container_set_num_blocks,
-    #   [CramContainer, :int32],
-    #   :void
+    attach_function \
+      :cram_container_set_num_blocks,
+      [:cram_container, :int32],
+      :void
 
-    # attach_function \
-    #   :cram_container_get_landmarks,
-    #   [CramContainer, :int32],
-    #   :pointer
+    attach_function \
+      :cram_container_get_landmarks,
+      [:cram_container, :int32],
+      :pointer
 
-    # attach_function \
-    #   :cram_container_set_landmarks,
-    #   [CramContainer, :Int32, :pointer],
-    #   :void
+    attach_function \
+      :cram_container_set_landmarks,
+      [:cram_container, :int32, :pointer],
+      :void
 
     attach_function \
       :cram_container_is_empty,
       [:cram_fd],
       :int
 
-    # attach_function \
-    #   :cram_block_get_content_id,
-    #   [CramBlock],
-    #   :int32
-    #
-    # attach_function \
-    #   :cram_block_get_comp_size,
-    #   [CramBlock],
-    #   :int32
-    #
-    # attach_function \
-    #   :cram_block_get_uncomp_size,
-    #   [CramBlock],
-    #   :int32
-    #
-    # attach_function \
-    #   :cram_block_get_crc32,
-    #   [CramBlock],
-    #   :int32
-    #
-    # attach_function \
-    #   :cram_block_get_data,
-    #   [CramBlock],
-    #   :pointer
-    #
+    attach_function \
+      :cram_block_get_content_id,
+      [:cram_block],
+      :int32
+    
+    attach_function \
+      :cram_block_get_comp_size,
+      [:cram_block],
+      :int32
+    
+    attach_function \
+      :cram_block_get_uncomp_size,
+      [:cram_block],
+      :int32
+    
+    attach_function \
+      :cram_block_get_crc32,
+      [:cram_block],
+      :int32
+    
+    attach_function \
+      :cram_block_get_data,
+      [:cram_block],
+      :pointer
+    
     # attach_function \
     #   :cram_block_get_content_type,
-    #   [CramBlock],
+    #   [:cram_block],
     #   CramContentType                # ?
-    #
+    
+    attach_function \
+      :cram_block_set_content_id,
+      [:cram_block, :int32],
+      :void
+   
+    attach_function \
+      :cram_block_set_comp_size,
+      [:cram_block, :int32],
+      :void
+    
+    attach_function \
+      :cram_block_set_uncomp_size,
+      [:cram_block, :int32],
+      :void
+    
+    attach_function \
+      :cram_block_set_crc32,
+      [:cram_block, :int32],
+      :void
+    
+    attach_function \
+      :cram_block_set_data,
+      [:cram_block, :pointer],
+      :void
+    
+    attach_function \
+      :cram_block_append,
+      [:cram_block, :pointer, :int],
+      :int
+    
+    attach_function \
+      :cram_block_update_size,
+      [:cram_block],
+      :void
+    
+    attach_function \
+      :cram_block_get_offset,
+      [:cram_block],
+      :size_t
+    
+    attach_function \
+      :cram_block_set_offset,
+      [:cram_block, :size_t],
+      :void
+    
+    attach_function \
+      :cram_block_size,
+      [:cram_block],
+      :uint32
+    
+    attach_function \
+      :cram_transcode_rg,
+      [:cram_fd, :cram_fd, :cram_container, :int, :pointer, :pointer],
+      :int
+    
+    attach_function \
+       :cram_copy_slice,
+      [:cram_fd, :cram_fd, :int32],
+      :int
+    
     # attach_function \
-    #   :cram_block_set_content_id,
-    #   [CramBlock, :int32],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_block_set_comp_size,
-    #   [CramBlock, :int32],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_block_set_uncomp_size,
-    #   [CramBlock, :int32],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_block_set_crc32,
-    #   [CramBlock, :int32],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_block_set_data,
-    #   [CramBlock, :pointer],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_block_append,
-    #   [CramBlock, :pointer, :int],
-    #   :int
-    #
-    # attach_function \
-    #   :cram_block_update_size,
-    #   [CramBlock],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_block_get_offset,
-    #   [CramBlock],
-    #   :size_t
-    #
-    # attach_function \
-    #   :cram_block_set_offset,
-    #   [CramBlock, :size_t],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_block_size,
-    #   [CramBlock],
-    #   :uint32
-    #
-    # attach_function \
-    #   :cram_transcode_rg,
-    #   [:cram_fd, :cram_fd, CramContainer, :int, :pointer, :pointer],
-    #   :int
-    #
-    # attach_function \
-    #   :cram_copy_slice,
-    #   [:cram_fd, :cram_fd, :int32],
-    #   :int
-    #
-    # attach_function \
-    #   :cram_new_block,
+    #  :cram_new_block,
     #   [CramContentType, :int],
-    #   CramBlock
-    #
-    # attach_function \
-    #   :cram_read_block,
-    #   [:cram_fd],
-    #   CramBlock
-    #
-    # attach_function \
-    #   :cram_write_block,
-    #   [:cram_fd, CramBlock],
-    #   :int
-    #
-    # attach_function \
-    #   :cram_free_block,
-    #   [CramBlock],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_uncompress_block,
-    #   [CramBlock],
-    #   :int
-    #
+    #   :cram_block
+    
+    attach_function \
+      :cram_read_block,
+      [:cram_fd],
+      :cram_block
+    
+    attach_function \
+      :cram_write_block,
+      [:cram_fd, :cram_block],
+      :int
+    
+    attach_function \
+      :cram_free_block,
+      [:cram_block],
+      :void
+    
+    attach_function \
+      :cram_uncompress_block,
+      [:cram_block],
+      :int
+    
     # attach_function \
     #   :cram_compress_block,
-    #   [:cram_fd, CramBlock, CramMetrics, :int, :int],
+    #   [:cram_fd, :cram_block, CramMetrics, :int, :int],
     #   :int
     #
     # attach_function \
     #   :cram_compress_block2,
-    #   [:cram_fd, CramSlice, CramBlock, CramMetrics, :int, :int],
+    #   [:cram_fd, CramSlice, :cram_block, CramMetrics, :int, :int],
     #   :int
-    #
-    # attach_function \
-    #   :cram_new_container,
-    #   [:int, :int],
-    #   CramContainer
-    #
-    # attach_function \
-    #   :cram_free_container,
-    #   [CramContainer],
-    #   :void
-    #
-    # attach_function \
-    #   :cram_read_container,
-    #   [:cram_fd],
-    #   CramContainer
-    #
-    # attach_function \
-    #   :cram_write_container,
-    #   [:cram_fd, CramContainer],
-    #   :int
-    #
-    # attach_function \
-    #   :cram_store_container,
-    #   [:cram_fd, CramContainer, :string, :pointer],
-    #   :int
-    #
-    # attach_function \
-    #   :cram_container_size,
-    #   [CramContainer],
-    #   :int
-    #
+    
+    attach_function \
+      :cram_new_container,
+      [:int, :int],
+      :cram_container
+    
+    attach_function \
+      :cram_free_container,
+      [:cram_container],
+      :void
+    
+    attach_function \
+      :cram_read_container,
+      [:cram_fd],
+      :cram_container
+    
+    attach_function \
+      :cram_write_container,
+      [:cram_fd, :cram_container],
+      :int
+    
+    attach_function \
+      :cram_store_container,
+      [:cram_fd, :cram_container, :string, :pointer],
+      :int
+    
+    attach_function \
+      :cram_container_size,
+      [:cram_container],
+      :int
+    
     attach_function \
       :cram_open,
       %i[string string],
--- scripts/FUNC_REPORT.md
+++ scripts/FUNC_REPORT.md
@@ -16,48 +16,16 @@
 < count attach_function
 ---
 > count HTSLIB_EXPORT
-10a11,16
-> cram_container_get_length
-> cram_container_set_length
-> cram_container_get_num_blocks
-> cram_container_set_num_blocks
-> cram_container_get_landmarks
-> cram_container_set_landmarks
-11a18,47
-> cram_block_get_content_id
-> cram_block_get_comp_size
-> cram_block_get_uncomp_size
-> cram_block_get_crc32
-> cram_block_get_data
+22a23
 > cram_block_get_content_type
-> cram_block_set_content_id
-> cram_block_set_comp_size
-> cram_block_set_uncomp_size
-> cram_block_set_crc32
-> cram_block_set_data
-> cram_block_append
-> cram_block_update_size
-> cram_block_get_offset
-> cram_block_set_offset
-> cram_block_size
-> cram_transcode_rg
-> cram_copy_slice
+34a36
 > cram_new_block
-> cram_read_block
-> cram_write_block
-> cram_free_block
-> cram_uncompress_block
+38a41
 > cram_compress_block
-> cram_new_container
-> cram_free_container
-> cram_read_container
-> cram_write_container
-> cram_store_container
-> cram_container_size
-17a54,55
+50a54,55
 > cram_set_option
 > cram_set_voption
-18a57,59
+51a57,59
 > cram_check_EOF
 > int32_put_blk
 > cram_get_refs
