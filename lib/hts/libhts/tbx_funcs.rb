e9ac2d889659f3038ef36a945a9215ffe5f95927
kojix2 <2xijok@gmail.com>
2022-08-19 12:03:37 +0900

    Add tbx_funcs

---
+++ lib/hts/libhts/tbx_funcs.rb
@@ -0,0 +1,25 @@
+module HTS
+  module LibHTS
+    class << self
+      def tbx_itr_destroy(iter)
+        hts_itr_destroy(iter)
+      end
+
+      def tbx_itr_queryi(tbx, tid, beg, end_)
+        hts_itr_query(tbx[:idx], tid, beg, end_, tbx_readrec)
+      end
+
+      def tbx_itr_querys(tbx, s)
+        hts_itr_querys(tbx[:idx], s, @@tbx_name2id, tbx, @@hts_itr_query, @@tbx_readrec)
+      end
+
+      def tbx_itr_next(htsfp, tbx, itr, r)
+        hts_itr_next(hts_get_bgzfp(htsfp), itr, r, tbx)
+      end
+
+      def tbx_bgzf_itr_next(bgzfp, tbx, itr, r)
+        hts_itr_next(bgzfp, itr, r, tbx)
+      end
+    end
+  end
+end
