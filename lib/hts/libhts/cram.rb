409f0dffb20dc18d2a2726b728eabe3aef4c9866
kojix2 <2xijok@gmail.com>
2022-08-22 05:01:20 +0000

    Add doc to libhts/cram

--- lib/hts/libhts/cram.rb
+++ lib/hts/libhts/cram.rb
@@ -7,6 +7,8 @@ module HTS
     typedef :pointer, :cram_block
     typedef :pointer, :cram_metrics
 
+    # cram_fd
+
     attach_function \
       :cram_fd_get_header,
       [:cram_fd],
@@ -81,6 +83,8 @@ module HTS
       :cram_container_is_empty,
       [:cram_fd],
       :int
+
+    # cram_block
 
     attach_function \
       :cram_block_get_content_id,
@@ -157,71 +161,88 @@ module HTS
       %i[cram_block size_t],
       :void
 
+    # Computes the size of a cram block, including the block header itself.
     attach_function \
       :cram_block_size,
       [:cram_block],
       :uint32
 
+    # Renumbers RG numbers in a cram compression header.
     attach_function \
       :cram_transcode_rg,
       %i[cram_fd cram_fd cram_container int pointer pointer],
       :int
 
+    # Copies the blocks representing the next num_slice slices from a
+    # container from 'in' to 'out'.
     attach_function \
       :cram_copy_slice,
       %i[cram_fd cram_fd int32],
       :int
 
+    # Returns the number of cram blocks within this slice.
     attach_function \
       :cram_slice_hdr_get_num_blocks,
       [:pointer],
       :int32
 
+    # Returns the block content_id for the block containing an embedded
+    # reference sequence.
     attach_function \
       :cram_slice_hdr_get_embed_ref_id,
       [:pointer],
       :int
 
+    # Returns slice reference ID, start and span (length) coordinates.
     attach_function \
       :cram_slice_hdr_get_coords,
       %i[pointer pointer pointer pointer],
       :void
 
+    # Decodes a slice header from a cram block.
     attach_function \
       :cram_decode_slice_header,
       %i[pointer pointer],
       :pointer
 
+    # Frees a cram_block_slice_hdr structure.
     attach_function \
       :cram_free_slice_header,
       [:pointer],
       :void
 
+    # Allocates a new cram_block structure with a specified content_type
+    # and id.
     attach_function \
       :cram_new_block,
       [CramContentType, :int],
       :cram_block
 
+    # Reads a block from a cram file.
     attach_function \
       :cram_read_block,
       [:cram_fd],
       :cram_block
 
+    # Writes a CRAM block.
     attach_function \
       :cram_write_block,
       %i[cram_fd cram_block],
       :int
 
+    # Frees a CRAM block, deallocating internal data too.
     attach_function \
       :cram_free_block,
       [:cram_block],
       :void
 
+    # Uncompresses a CRAM block, if compressed.
     attach_function \
       :cram_uncompress_block,
       [:cram_block],
       :int
 
+    # Compresses a block.
     attach_function \
       :cram_compress_block,
       %i[cram_fd cram_block cram_metrics int int],
@@ -232,6 +253,8 @@ module HTS
     #   %i[cram_fd cram_slice cram_block cram_metrics int int],
     #   :int
 
+    # Creates a new container, specifying the maximum number of slices
+    # and records permitted.
     attach_function \
       :cram_new_container,
       %i[int int],
@@ -242,16 +265,20 @@ module HTS
       [:cram_container],
       :void
 
+    # Reads a container header.
     attach_function \
       :cram_read_container,
       [:cram_fd],
       :cram_container
 
+    # Writes a container structure.
     attach_function \
       :cram_write_container,
       %i[cram_fd cram_container],
       :int
 
+    # Stores the container structure in dat and returns *size as the
+    # number of bytes written to dat[].
     attach_function \
       :cram_store_container,
       %i[cram_fd cram_container string pointer],
@@ -262,61 +289,73 @@ module HTS
       [:cram_container],
       :int
 
+    # Opens a CRAM file for read (mode "rb") or write ("wb").
     attach_function \
       :cram_open,
       %i[string string],
       :cram_fd
 
+    # Opens an existing stream for reading or writing.
     attach_function \
       :cram_dopen,
       %i[pointer string string],
       :cram_fd
 
+    # Closes a CRAM file.
     attach_function \
       :cram_close,
       [:cram_fd],
       :int
 
+    # Seek within a CRAM file.
     attach_function \
       :cram_seek,
       %i[pointer off_t int],
       :int # FIXME: pointer should be :cram_fd
 
+    # Flushes a CRAM file.
     attach_function \
       :cram_flush,
       [:cram_fd],
       :int
 
+    # Checks for end of file on a cram_fd stream.
     attach_function \
       :cram_eof,
       [:cram_fd],
       :int
 
+    # Sets options on the cram_fd.
     attach_function \
       :cram_set_option,
       [:cram_fd, HtsFmtOption, :varargs],
       :int
 
+    # Sets options on the cram_fd.
     attach_function \
       :cram_set_voption,
       [:cram_fd, HtsFmtOption, :pointer], # va_list
       :int
 
+    # Attaches a header to a cram_fd.
     attach_function \
       :cram_set_header,
       [:cram_fd, SamHdr.by_ref],
       :int
 
+    # Check if this file has a proper EOF block
     attach_function \
       :cram_check_EOF,
       [:cram_fd],
       :int
 
+    # As int32_decoded/encode, but from/to blocks instead of cram_fd
     attach_function \
       :int32_put_blk,
       %i[cram_block int32_t],
       :int
 
+    # Returns the refs_t structure used by a cram file handle.
     attach_function \
       :cram_get_refs,
       [HtsFile.by_ref],
