2bb065b61a78a7c728a90559c9eebaca5666c68d
kojix2 <2xijok@gmail.com>
2022-10-13 11:44:58 +0900

    Add header record skeleton

---
+++ lib/hts/bcf/header/record.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module HTS
+  class Bcf < Hts
+    class Header
+      class Record
+        def initialize
+          @bcf_hrec
+        end
+      end
+    end
+  end
+end
