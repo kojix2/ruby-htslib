7d6ef6a9ac950a67df55c826804a8bdc1e7ae1b9
kojix2 <2xijok@gmail.com>
2023-01-10 18:02:55 +0900

    Add tabix example

---
+++ examples/tabix.rb
@@ -0,0 +1,12 @@
+# Using tabix with dbVar GVF and VCF FTP Files
+# https://www.ncbi.nlm.nih.gov/dbvar/content/tools/tabix/
+
+require "htslib"
+
+vcfurl = "https://ftp.ncbi.nlm.nih.gov/pub/dbVar/data/Homo_sapiens/by_study/vcf/nstd102.GRCh38.variant_region.vcf.gz"
+tb = HTS::Tabix.open(vcfurl)
+
+# query chr1 from 1 to 1000000\
+tb.query("1", 1, 1_000_000) do |r|
+  puts r.join("\t")
+end
