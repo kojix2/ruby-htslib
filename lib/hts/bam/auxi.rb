bf77d9cca842064083c2f71513cf6314bc7e4c4f
kojix2 <2xijok@gmail.com>
2023-05-25 12:31:53 +0900

    Fix variable name

--- lib/hts/bam/auxi.rb
+++ lib/hts/bam/auxi.rb
@@ -30,10 +30,10 @@ module HTS
       # which provides methods like `get_int`, `get_float`, etc.
       # I think they are better than `fetch_int`` and `fetch_float`.
       def get(key, type = nil)
-        aux = LibHTS.bam_aux_get(@record.struct, key)
-        return nil if aux.null?
+        aux_ptr = LibHTS.bam_aux_get(@record.struct, key)
+        return nil if aux_ptr.null?
 
-        get_ruby_aux(aux, type)
+        get_ruby_aux(aux_ptr, type)
       end
 
       # For compatibility with HTS.cr.
@@ -56,43 +56,47 @@ module HTS
       end
 
       def first
-        aux = LibHTS.bam_aux_first(@record.struct)
-        return nil if aux.null?
+        aux_ptr = first_pointer
+        return nil if aux_ptr.null?
 
-        get_ruby_aux(aux)
+        get_ruby_aux(aux_ptr)
       end
 
       def each
         return enum_for(__method__) unless block_given?
 
-        aux = LibHTS.bam_aux_first(@record.struct)
-        return nil if aux.null?
+        aux_ptr = first_pointer
+        return nil if aux_ptr.null?
 
         loop do
-          yield get_ruby_aux(aux)
-          aux = LibHTS.bam_aux_next(@record.struct, aux)
-          break if aux.null?
+          yield get_ruby_aux(aux_ptr)
+          aux_ptr = LibHTS.bam_aux_next(@record.struct, aux_ptr)
+          break if aux_ptr.null?
         end
       end
 
       def to_h
         h = {}
-        aux = LibHTS.bam_aux_first(@record.struct)
-        return h if aux.null?
+        aux_ptr = first_pointer
+        return h if aux_ptr.null?
 
         loop do
-          key = FFI::Pointer.new(aux.address - 2).read_string(2)
-          h[key] = get_ruby_aux(aux)
-          aux = LibHTS.bam_aux_next(@record.struct, aux)
-          break if aux.null?
+          key = FFI::Pointer.new(aux_ptr.address - 2).read_string(2)
+          h[key] = get_ruby_aux(aux_ptr)
+          aux_ptr = LibHTS.bam_aux_next(@record.struct, aux_ptr)
+          break if aux_ptr.null?
         end
         h
       end
 
       private
 
-      def get_ruby_aux(aux, type = nil)
-        type = type ? type.to_s : aux.read_string(1)
+      def first_pointer
+        LibHTS.bam_aux_first(@record.struct)
+      end
+
+      def get_ruby_aux(aux_ptr, type = nil)
+        type = type ? type.to_s : aux_ptr.read_string(1)
 
         # A (character), B (general array),
         # f (real number), H (hexadecimal array),
@@ -100,23 +104,23 @@ module HTS
 
         case type
         when "i", "I", "c", "C", "s", "S"
-          LibHTS.bam_aux2i(aux)
+          LibHTS.bam_aux2i(aux_ptr)
         when "f", "d"
-          LibHTS.bam_aux2f(aux)
+          LibHTS.bam_aux2f(aux_ptr)
         when "Z", "H"
-          LibHTS.bam_aux2Z(aux)
+          LibHTS.bam_aux2Z(aux_ptr)
         when "A" # char
-          LibHTS.bam_aux2A(aux).chr
+          LibHTS.bam_aux2A(aux_ptr).chr
         when "B" # array
-          t2 = aux.read_string(2)[1] # just a little less efficient
-          l = LibHTS.bam_auxB_len(aux)
+          t2 = aux_ptr.read_string(2)[1] # just a little less efficient
+          l = LibHTS.bam_auxB_len(aux_ptr)
           case t2
           when "c", "C", "s", "S", "i", "I"
             # FIXME : Not efficient.
-            Array.new(l) { |i| LibHTS.bam_auxB2i(aux, i) }
+            Array.new(l) { |i| LibHTS.bam_auxB2i(aux_ptr, i) }
           when "f", "d"
             # FIXME : Not efficient.
-            Array.new(l) { |i| LibHTS.bam_auxB2f(aux, i) }
+            Array.new(l) { |i| LibHTS.bam_auxB2f(aux_ptr, i) }
           else
             raise NotImplementedError, "type: #{type} #{t2}"
           end
